97ec2118911d1812c6f45d6a880f406601ce18b9
Manikandan-16 <Manikandan-16@users.noreply.github.com>
2018-06-07 16:12:52 +0530

    Update Palindrome.java

--- src/main/java/maven/Palindrome.java
+++ src/main/java/maven/Palindrome.java
@@ -1,69 +1,63 @@
 package hello;
 
-import java.util.*;
+class Palindrome {
 
-import java.lang.*;
+	static boolean pal(String str)		//method to check if the passed String is palindrome or not
 
-import java.io.*;
+	{
 
-class Palindrome{
-	
-static boolean pal(String str)
-	
-{
+		int x = str.length();
+		int k = x - 1;
 
-int x=str.length();
-int k=x-1;
+		int i;
 
-int i;
+		for (i = 0; i < x; i++, k--) 		
 
-for(i=0;i<x;i++,k--)
+		{
 
-{
+			if (str.charAt(i) != str.charAt(k))
 
-if(str.charAt(i)!=str.charAt(k))
+				return false;
 
-return false;
+		}
 
-}
+		return true;
 
-return true;
+	}
 
-}
+	public static void main(String[] args) throws java.lang.Exception
 
-public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
 
-{
-Scanner sc=new Scanner(System.in);
-String s=sc.nextLine();
+		int i, n = s.length(), count = 0, j;
 
-int i,n=s.length(),count=0,j;
+		for (i = 2; i <= n; i++)		//checks for palindrome words for length more than 1
 
-for(i=2;i<=n;i++)
+		{
 
-{
+			for (j = 0; j <= n - i; j++)
 
-for(j=0;j<=n-i;j++)
+			{
+				if (pal(s.substring(j, j + i)))
 
-{
-if(pal(s.substring(j,j+i)))
+				{
 
-{
+					System.out.println(s.substring(j, j + i));
 
-System.out.println(s.substring(j,j+i));
+					count++;
 
-count++;
+				}
 
-}
+				else
 
-else
+					continue;
 
-continue;
+			}
+		}
 
-}
-}
-
-System.out.println(count);
-
-}
+		System.out.println(count);	//print the total possible value
+		sc.close();
+	}
 }
